d52a4156fee2245e34bc2a20611429e4afd7e3c6
Sergey <47007006+sergeys177@users.noreply.github.com>
2019-02-02 23:30:20 +0300

    Update hw2.sql

--- HomeWork/hw2.sql
+++ HomeWork/hw2.sql
@@ -1,28 +1,31 @@
 SELECT 'ФИО: Скурихин Сергей';
 -- первый запрос
-SELECT * FROM ratings LIMIT 10;
+SELECT * 
+    FROM ratings 
+    LIMIT 10;
 -- второй запрос
-SELECT * FROM links
+SELECT * 
+FROM links
 WHERE
     movieid between 100 and 1000
     AND imdbid LIKE '%42'
-    LIMIT 10;
+LIMIT 10;
 -- третий запрос
 SELECT imdbId
-FROM links
+     FROM links
      JOIN ratings
      ON links.movieid = ratings.movieid
-WHERE rating = 5
+     WHERE rating = 5
 LIMIT 10;
 -- четвертый запрос
 SELECT COUNT(*)
 FROM links
     WHERE imdbid IS NULL;
 -- пятый запрос
-    SELECT
-        userId,
-        AVG(rating) as avg_rating
+SELECT userId,
+       AVG(rating) as avg_rating 
     FROM public.ratings
-    GROUP BY userId, rating DESC
+    GROUP BY userId
     HAVING AVG(rating) > 3.5
-    LIMIT 10;
+    ORDER BY avg_rating DESC
+LIMIT 10;
