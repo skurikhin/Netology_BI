b568c1f77e2ba961b94730db6fa24acaa81f0085
Sergey <47007006+sergeys177@users.noreply.github.com>
2019-02-02 22:03:56 +0300

    Add files via upload

--- HomeWork/hw2.sql
+++ HomeWork/hw2.sql
@@ -7,3 +7,8 @@ WHERE
     r1.movieid between "100" and "1000"
     AND r2.imdbld LIKE "%42"
     LIMIT 10;
+-- третий запрос
+SELECT * FROM links
+JOIN ratings
+    ON links=ratings
+    WHERE imdbld = 5
