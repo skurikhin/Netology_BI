6ed400130079348f53d556ca94b0586e6663edb7
Sergey <47007006+sergeys177@users.noreply.github.com>
2019-02-03 09:50:09 +0300

    Add files via upload

--- HomeWork/hw2.sql
+++ HomeWork/hw2.sql
@@ -1,31 +1,40 @@
 SELECT 'ФИО: Скурихин Сергей';
 -- первый запрос
-SELECT * 
-    FROM ratings 
-    LIMIT 10;
+SELECT * FROM ratings LIMIT 10;
 -- второй запрос
-SELECT * 
-FROM links
+SELECT * FROM links
 WHERE
     movieid between 100 and 1000
     AND imdbid LIKE '%42'
-LIMIT 10;
+    LIMIT 10;
+-- СЛОЖНЫЕ ВЫБОРКИ
 -- третий запрос
 SELECT imdbId
-     FROM links
+FROM links
      JOIN ratings
      ON links.movieid = ratings.movieid
-     WHERE rating = 5
+WHERE rating = 5
 LIMIT 10;
 -- четвертый запрос
 SELECT COUNT(*)
 FROM links
-    WHERE imdbid IS NULL;
+  WHERE imdbid IS NULL;
 -- пятый запрос
-SELECT userId,
-       AVG(rating) as avg_rating 
-    FROM public.ratings
-    GROUP BY userId
-    HAVING AVG(rating) > 3.5
-    ORDER BY avg_rating DESC
+SELECT
+    userId,
+    AVG(rating) as avg_rating
+  FROM public.ratings
+  GROUP BY userId, rating DESC
+  HAVING AVG(rating) > 3.5
+LIMIT 10;
+-- ИЕРАРХИЧЕСКИЕ ЗАПРОСЫ
+-- шестой запрос
+SELECT imdbId
+    FROM links
+    WHERE movieid (
+      SELECT movieid
+      FROM ratings
+GROUP BY movieid ORDER BY random() LIMIT 1
+HAVING AVG(rating) > 3.5)
+    )
 LIMIT 10;
