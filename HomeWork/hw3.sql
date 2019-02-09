40b07515b9b216e9103a62c09814843b6ca43d36
Sergey <47007006+sergeys177@users.noreply.github.com>
2019-02-09 23:23:28 +0300

    Update hw3.sql

--- HomeWork/hw3.sql
+++ HomeWork/hw3.sql
@@ -3,8 +3,9 @@ SELECT 'ФИО: Скурихин Сергей';
 -- Оконные функции.
 
 SELECT userId, movieId, rating,
-	(rating - MIN(rating) OVER (PARTITION BY userId))/(MAX(rating) OVER 		(PARTITION BY userId) - MIN(rating) OVER
-	(PARTITION BY userId)) as normed_rating,
+	(rating - MIN(rating) OVER (PARTITION BY userId))/(MAX(rating) 
+	OVER (PARTITION BY userId) - MIN(rating) 
+	OVER (PARTITION BY userId)) as normed_rating,
   	AVG(rating) OVER (PARTITION BY userId) as avg_rating
 FROM (
     	SELECT DISTINCT userId, movieId, rating
